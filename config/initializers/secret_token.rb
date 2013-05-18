df2a683bb4386e1b65eb88dad106695754949091
Ranjeet Maurya <askranjeetmaurya@gmail.com>
2013-05-18 10:42:13 +0530

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HealthCheckUp::Application.config.secret_token = '3643968d135f37417635d198c30670ef67723041c2e33e8e9d9fe4774d5fc961a11a559c94ffa37594f3f7b16ac011097ac4c4ca689bdd7c9d8b868ff726ebfa'
